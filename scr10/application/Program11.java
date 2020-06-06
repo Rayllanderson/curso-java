0a335627169d8eba1424244784b8f988f4d2506c
Rayllanderson <rayllanderson@gmail.com>
2020-06-06 08:17:25 -0300

    Account finalizado

--- scr10/application/Program11.java
+++ scr10/application/Program11.java
@@ -1,9 +1,47 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import model.entities.*;
+import model.exceptions.BalanceException;
+
 public class Program11 {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	Scanner scan = new Scanner(System.in);
+
+	System.out.println(" \tAbrindo uma conta ");
+	System.out.print("Nome: ");
+	try {
+	    String nome = scan.nextLine();
+	    System.out.print("Renda Mensal: R$");
+	    double rendaMensal = scan.nextDouble();
+
+	    Account ac = new Account(15989, nome, 0.0, rendaMensal);
+	    System.out.print("Valor a depositar: R$");
+	    double valor = scan.nextDouble();
+	    ac.deposit(valor);
+	    System.out.print("Valor a sacar: R$");
+	    valor = scan.nextDouble();
+	    ac.withdraw(valor);
+	    
+	    System.out.println();
+	    
+	    System.out.println(ac);
+
+	} catch (BalanceException e) {
+	    System.out.println("Desculpe, " + e.getMessage());
+	    
+	} catch (InputMismatchException e) {
+	    System.out.println("Tente digita um número válido.");
+	    
+	} catch (RuntimeException e) {
+	    System.out.println("ops... ocorreu um erro inesperado :/");
+	    
+	} finally {
+	    scan.close();
+	    
+	}
 
     }
 
--- scr10/model/entities/Account.java
+++ scr10/model/entities/Account.java
@@ -1,5 +1,97 @@
 package model.entities;
 
+import model.exceptions.BalanceException;
+
 public class Account {
+    
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+    
+    
+    public Account() {
+	
+    }
+
+    public Account(Integer number, String holder, Double balance, Double rendaMensal) {
+	this.number = number;
+	this.holder = holder;
+	this.balance = balance;
+	this.setWithdrawLimit(rendaMensal);
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double rendaMensal) {
+        if (rendaMensal <= 1000.00) {
+            this.withdrawLimit = 800.00;
+        }else if (rendaMensal <= 3000.00) {
+            this.withdrawLimit = 2800.00;
+        }else if (rendaMensal <= 5000.00) {
+            this.withdrawLimit = 4800.00;
+        }else
+            this.withdrawLimit = 10000.00;
+    }
+    
+    public void deposit(double amount) {
+	this.balance += amount;
+	this.newBalance();
+    }
+    
+    public void withdraw(double amount) {
+	if (amount > this.withdrawLimit) {
+	    throw new BalanceException("o valor a sacar maior que o limite");
+	}
+	if (this.balance == 0) {
+	    throw new BalanceException("a conta está sem saldo disponível");
+	}
+	
+	this.balance -= amount;
+	
+	this.newBalance();
+	
+    }
+    
+    private void newBalance() {
+	System.out.println("Novo saldo: " + this.balance);
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder s = new StringBuilder();
+	s.append("Número da conta: " + this.number);
+	s.append("\n" + this.holder);
+	s.append("\nSaldo: R$" + this.balance + "\nSeu limite de saque é de: R$" + this.withdrawLimit + "\n");
+	
+	return s.toString();
+    }
+    
+    
+    
+    
+    
 
 }
